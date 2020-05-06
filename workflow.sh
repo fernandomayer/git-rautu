217e2c23f74487186344efb8c3f469911061d6a4
Fernando Mayer <fernandomayer@gmail.com>
2020-05-06 17:18:58 -0300

    Inclui remotes

--- workflow.sh
+++ workflow.sh
@@ -30,6 +30,16 @@ git status
 git log
 git log --oneline
 
+# Adiciona repositório remoto
+# No GitHub crie um repositório com o mesmo nome deste diretório
+# Adicione o endereço remoto como origin
+git remote add origin git@github.com:fernandomayer/primeiro.git
+git remote show
+git status
+# Envia (PUSH) o repositório local para o remoto
+git push -u origin master
+git status
+
 # Trabalhando com branches
 # Verifica os branches atuais
 git branch
@@ -61,6 +71,13 @@ git status
 git log --oneline
 git diff master...novo
 cat README.md
+
+# Note que tudo isso foi feito localmente. Para enviar para o servidor
+# use o push
+git status
+git push origin master
+git status
+
 # Deleta o ramo novo
 git branch
 git branch -d novo
@@ -70,4 +87,7 @@ git branch
 nano README.md
 git status
 git commit -am 'Ultima edição'
+git status
+# Envia para o repositório
+git push origin master
 git status
